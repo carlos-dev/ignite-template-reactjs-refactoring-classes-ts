87a82cae6ec8df0362980dcebd6f10c6277c03a7
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-10-30 12:02:19 -0300

    refatorando component Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,9 +9,14 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+type Props = {
+  name: string;
+  icon?: React.ComponentType<{ size: number }>;
+}
 
+const Input = ({ name, icon: Icon, ...rest }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  console.log(rest);
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
 
